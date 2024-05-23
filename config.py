d1c39aca377fe57d268cc5ab0bb221ff4428b76e
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-23 15:27:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rajputshivsingh65/DNS-MUSIC",
+    "BHAG YEH SA MATHERCHOD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
